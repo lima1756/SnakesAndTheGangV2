a62f1ddbaae73e903366cb41a62aff9cec04c3b5
AlexGz9851 <a00820225@itesm.mx>
2021-04-02 14:17:40 -0600

    negamax is working

--- snake.py
+++ snake.py
@@ -27,12 +27,15 @@ class Snake:
         self.direction = [0, 0]
         self.directionKey = 0
         self.alive = True
+        self.growing = False
 
     def grow(self):
         self.length += 1
+        self.growing = True
         self.body.append(Point(self.body[0].x, self.body[0].y))
 
     def move(self, limitX, limitY, block_size):
+        self.growing = False
         for i in range(self.length-1, 0, -1):
             self.body[i] = self.body[i-1]
         self.body[0] = Point(self.body[0].x + self.direction[0],
@@ -55,6 +58,15 @@ class Game(TwoPlayersGame):
     display = None
     clock = None
     font_style = None
+    DIRECTIONS = {
+        pygame.K_LEFT: [-BLOCK_SIZE, 0],
+        pygame.K_RIGHT: [BLOCK_SIZE, 0],
+        pygame.K_UP: [0, -BLOCK_SIZE],
+        pygame.K_DOWN: [0, BLOCK_SIZE]
+    }
+
+    OPPOSITE_MOVE = {pygame.K_RIGHT: pygame.K_LEFT, pygame.K_UP: pygame.K_DOWN,
+                    pygame.K_LEFT: pygame.K_RIGHT, pygame.K_DOWN: pygame.K_UP}
 
 
     def __init__(self, players):
@@ -111,22 +123,27 @@ class Game(TwoPlayersGame):
         else:
             self.food.append(f)
 
-    def collissionFood(self):
+    def collision_food(self):
         for snake in self.snakes:
             for i in range(len(self.food)):
                 if self.food[i].x == snake.body[0].x and self.food[i].y == snake.body[0].y:
                     snake.grow()
                     self.addFood(i)
 
-    def collissionSnakes(self):
+    def check_collision(self, i, j):
+        start = 1 if i == j else 0 # avoid head check herself.
+        for b in range(start, len(self.snakes[j].body)):
+            if (not (i == j and self.snakes[i].growing)) and self.snakes[i].body[0].x == self.snakes[j].body[b].x \
+                    and self.snakes[i].body[0].y == self.snakes[j].body[b].y:
+                    return True
+        return False
+
+    def collision_snakes(self):
         total = len(self.snakes)
         death = []
         for i in range(total):
             for j in range(total):
-                if i == j:
-                    continue
-                for point in self.snakes[j].body:
-                    if self.snakes[i].body[0].x == point.x and self.snakes[i].body[0].y == point.y:
+                if self.check_collision(i, j):
                         death.append(i)
         for i in death:
             self.snakes[i].alive = False
@@ -137,20 +154,33 @@ class Game(TwoPlayersGame):
     def points_over_diamonds(self, indexPlayer, weight=0.3):
         scores = []
         for f in self.food:
-            scores.append((1 - (abs(self.snakes[indexPlayer].body[0].y - f.y) / self.HEIGHT)) +
-                          (1 - (abs(self.snakes[indexPlayer].body[0].x - f.x) / self.WIDTH)))
+            dy = abs(self.snakes[indexPlayer].body[0].y - f.y)
+            dx = abs(self.snakes[indexPlayer].body[0].x - f.x)
+            dy = self.HEIGHT - dy if dy > self.HEIGHT / 2 else dy
+            dx = self.WIDTH - dx if dx > self.WIDTH / 2 else dx
+
+            distance = (dy/self.HEIGHT) + (dx/self.WIDTH)
+            score = 1 - distance
+
+            scores.append(score)
         scores = sorted(scores, reverse=True)
 
-        return (scores[0] + scores[1]*.606 +  scores[2] * .13)  * weight * self.FOOD_QTY
+        return (scores[0] + scores[1] *.5 + scores[2] *.125) *weight
 
-    def points_over_enemy(self, indexPlayer, weight=.5):
+    def points_over_enemy(self, indexPlayer, weight=.1):
         score = 0
         opponent = (indexPlayer + 1) % 2
-        weight_due_lenght = 3 # TODO
+        weight_due_lenght = 3 / (len(self.snakes[indexPlayer].body)**(0.8))
         for b in self.snakes[indexPlayer].body:
-            distance = (1 - (abs(self.snakes[opponent].body[0].y - b.y) / self.HEIGHT)) + \
-                     (1 - (abs(self.snakes[opponent].body[0].x - b.x) / self.WIDTH))
-            score += m.log1p(m.e**(-distance*distance/2))
+
+            dx = abs(b.x - self.snakes[opponent].body[0].x)
+            dx = self.WIDTH - dx  if dx > self.WIDTH /2 else dx
+
+            dy = abs(b.y - self.snakes[opponent].body[0].y)
+            dy = self.HEIGHT - dy  if dy > self.HEIGHT /2 else dy
+            distance = (dy / self.HEIGHT) + (dx / self.WIDTH)
+
+            score += m.log1p(m.e**(2 - distance))
         return score*weight * weight_due_lenght
 
     def scoring(self):
@@ -160,40 +190,37 @@ class Game(TwoPlayersGame):
             score = 100
         else:
             score = 0
-            score += self.points_over_diamonds(0)
-            score -= self.points_over_diamonds(1)
-            score += self.points_over_enemy(0)
-            score -= self.points_over_enemy(1)
+            score += self.points_over_diamonds(0, weight=1)
+            score -= self.points_over_diamonds(1, weight=1)
+            score += self.points_over_enemy(0, weight=.2)
+            score -= self.points_over_enemy(1, weight=.2)
+            print(score)
         return score
 
+    def check_avoid_auto_collision(self, move):
+        snake = self.snakes[self.nplayer - 1]
+        for b in range(1, len(snake.body)):
+            if (not snake.growing) and \
+                    snake.body[0].x + self.DIRECTIONS[move][0] == snake.body[b].x and \
+                    snake.body[0].y + self.DIRECTIONS[move][1] == snake.body[b].y:
+                    return False
+        return True
+
     def possible_moves(self):
-        """TODO cosas raras pasan aqui.
-            Ademas de considerar el añadir restricciones de movimientos
-            como dice Ivan, ( que no estoy de acuerdo, porque las colisiones
-            se consideran en el scoring).
-            El modelo que ofrezco actualmente es defectuoso, o al menos negamax
-            toma mal los movimientos, en el sentido de que, si se añade la direccion
-            hacia adelante ( o no hacer nada, lo cual es equivalente), decide ir en
-            linea recta.
-            Si no se le añade esa opcion nula intercala entre movimientos arriba y derecha.
-        """
-        moves = [pygame.K_RIGHT, pygame.K_UP, pygame.K_LEFT, pygame.K_DOWN]
-        return [m for m in moves if self.snakes[self.nplayer - 1].directionKey != m ]
-        # return [m if self.snakes[self.nplayer - 1].directionKey != m else 0 for m in moves]
+        direction = self.snakes[self.nplayer - 1].directionKey
+        possible_moves = [self.OPPOSITE_MOVE[d] for d in self.OPPOSITE_MOVE if direction != d]
+        possible_moves = [mm for mm in possible_moves if self.check_avoid_auto_collision(mm)]
+        if len(possible_moves) == 0:
+            possible_moves = [self.OPPOSITE_MOVE[d] for d in self.OPPOSITE_MOVE if direction != d]
+            # doesn't matter this case, is dead already, only avoiding bug on negamax.
+        return possible_moves
 
     def make_move(self, move):
-        if move == pygame.K_LEFT and self.snakes[self.nplayer-1].directionKey != pygame.K_RIGHT:
-            self.snakes[self.nplayer-1].direction = [-self.BLOCK_SIZE, 0]
-            self.snakes[self.nplayer-1].directionKey = move
-        elif move == pygame.K_RIGHT and self.snakes[0].directionKey != pygame.K_LEFT:
-            self.snakes[self.nplayer-1].direction = [self.BLOCK_SIZE, 0]
-            self.snakes[self.nplayer-1].directionKey = move
-        elif move == pygame.K_UP and self.snakes[self.nplayer-1].directionKey != pygame.K_DOWN:
-            self.snakes[self.nplayer-1].direction = [0,-self.BLOCK_SIZE]
-            self.snakes[self.nplayer-1].directionKey = move
-        elif move == pygame.K_DOWN and self.snakes[0].directionKey != pygame.K_UP:
-            self.snakes[self.nplayer-1].direction = [0, self.BLOCK_SIZE]
-            self.snakes[self.nplayer-1].directionKey = move
+        snake = self.snakes[self.nplayer-1]
+        if move in self.OPPOSITE_MOVE and snake.directionKey != self.OPPOSITE_MOVE[move]:
+            snake.direction = self.DIRECTIONS[move]
+            snake.directionKey = move
+        snake.move(self.WIDTH, self.HEIGHT, self.BLOCK_SIZE)
 
     def gameLoop(self):
         while not self.game_over:
@@ -233,15 +260,10 @@ class Game(TwoPlayersGame):
 
             # MOVING THE DUMMY ENEMY
             ai_move = self.get_move()
-            self.play_move(ai_move)
-            # self.make_move(self.possible_moves()[random.randrange(0, 3)])
+            self.play_move(ai_move) # self.make_move(self.possible_moves()[random.randrange(0, 3)])
 
-            self.collissionFood()
-
-            for snake in self.snakes:
-                snake.move(self.WIDTH, self.HEIGHT, self.BLOCK_SIZE)
-                self.collissionSnakes()
-
+            self.collision_food()
+            self.collision_snakes()
             self.drawSnakes()
             self.drawFood()
 
@@ -252,7 +274,7 @@ class Game(TwoPlayersGame):
 
 
 def main():
-    Game([ Human_Player(), AI_Player(Negamax(7, win_score=100)) ]).gameLoop()
+    Game([ Human_Player(), AI_Player(Negamax(1, win_score=100)) ]).gameLoop()
 
 
 if __name__ == '__main__':
